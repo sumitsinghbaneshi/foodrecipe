b02c01dfd4bb6389bbb7d7eebb6539aa8dbf5326
sumitsinghbaneshi <sumitsinghbaneshi@gmail.com>
2022-05-22 03:49:16 +0530

    Updates

---
+++ precache-manifest.04e2ec5a27703dadcf3b85f2b2813027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7765d531a6a1ca39c34",
+    "url": "/foodrecipe/static/css/main.86edc470.chunk.css"
+  },
+  {
+    "revision": "e7765d531a6a1ca39c34",
+    "url": "/foodrecipe/static/js/main.e7765d53.chunk.js"
+  },
+  {
+    "revision": "7c4abfd82c740107af87",
+    "url": "/foodrecipe/static/js/1.7c4abfd8.chunk.js"
+  },
+  {
+    "revision": "fd740fe59d4ba9165f46",
+    "url": "/foodrecipe/static/js/runtime~main.fd740fe5.js"
+  },
+  {
+    "revision": "3fc49e3b61590d4c0c2e8c0d2177593b",
+    "url": "/foodrecipe/index.html"
+  }
+];
